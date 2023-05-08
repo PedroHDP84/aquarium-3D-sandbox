446d93143edca8fb18209e42945b4299b76638eb
Pedro <pedrohdp84@hotmail.com>
2023-05-08 00:59:42 -0300

    adding lights and text

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ import {
   WebGLRenderer,
   AnimationClip,
   GridHelper,
-  ConeGeometry,
-  MeshPhongMaterial,
-  Mesh,
+  PointLight,
+  PointLightHelper,
   Vector3,
   HemisphereLight,
 } from 'three'
@@ -19,16 +18,17 @@ const scrollElement = document.querySelector('.scroller')
 
 const scene = new Scene()
 const camera = new PerspectiveCamera(45, canvasParent.clientWidth / canvasParent.clientHeight, 0.1, 10000)
-const renderer = new WebGLRenderer()
+const renderer = new WebGLRenderer({alpha: true})
 renderer.setSize(canvasParent.clientWidth, canvasParent.clientHeight)
 canvasParent.appendChild(renderer.domElement)
 
 const light = new HemisphereLight(0xffffbb, 0x080820, 1)
+light.position.set(1, 1, 1)
 scene.add(light)
 
-const grid = new GridHelper(100, 50)
-grid.position.set(0, -5, 0)
-scene.add(grid)
+// const grid = new GridHelper(100, 50)
+// grid.position.set(0, -5, 0)
+// scene.add(grid)
 
 const rig = new CameraRig(camera, scene)
 rig.setAnimationClip(AnimationClip.parse(cameraData.animationClip))
@@ -73,16 +73,16 @@ controls3dof.enable()
 const cones = [
   {
     meshPosition: new Vector3(0, 0, -30),
-    fileName: 'AngelFish.glb',
-    scaleFactor: 1
+    fileName: 'DevilRay.glb',
+    scaleFactor: 3
   },
   {
     meshPosition: new Vector3(20, 0, -45),
-    fileName: 'Atolla.glb',
-    scaleFactor: 1
+    fileName: 'AngelFish.glb',
+    scaleFactor: .4
   },
   {
-    meshPosition: new Vector3(45, 0, 0),
+    meshPosition: new Vector3(12, 0, -10),
     fileName: 'Baby_Turtule.glb',
     scaleFactor: 1
   },
@@ -93,13 +93,13 @@ const cones = [
   },
   {
     meshPosition: new Vector3(-10, 0, 45),
-    fileName: 'DevilRay.glb',
+    fileName: 'Dolphin.glb',
     scaleFactor: 1
   },
   {
     meshPosition: new Vector3(-40, 0, 20),
-    fileName: 'Dolphin.glb',
-    scaleFactor: 1
+    fileName: 'HumpBack.glb',
+    scaleFactor: 2.5
   },
 ]
 
@@ -108,8 +108,18 @@ const loader = new GLTFLoader().setPath( 'models/' );
 cones.forEach((item) => {
   loader.load( item.fileName, function ( gltf ) {
     gltf.scene.position.copy(item.meshPosition)
+    gltf.scene.scale.setScalar(item.scaleFactor) // scale the baskeball
     scene.add( gltf.scene );
   } );
+
+
+  const pointLight = new PointLight(0xffffbb )
+  pointLight.position.copy(item.meshPosition)
+  pointLight.position.setY(3)
+  scene.add(pointLight)
+
+  // const spotLightHelper = new PointLightHelper( pointLight );
+  // scene.add( spotLightHelper );
 
 })
 
